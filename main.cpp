687ae808372382f79233727e2d2295b9e57a3821
TylerLi177 <95332938+TylerLi177@users.noreply.github.com>
2022-03-01 19:28:22 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@
 #include <FEHSD.h>
 #include <string.h>
 #include <stdio.h>
+#define testSpeed 25
+#define ninetyDegreeCount 135
+#define PI 3.14159265358979323846
 
 DigitalEncoder right_encoder(FEHIO::P1_0);
 DigitalEncoder left_encoder(FEHIO::P3_0);
@@ -19,6 +22,7 @@ AnalogInputPin cds(FEHIO::P0_1);
 AnalogInputPin left_opt(FEHIO::P2_7);
 AnalogInputPin middle_opt(FEHIO::P2_0);
 AnalogInputPin right_opt(FEHIO::P1_4);
+FEHMotor bucket_arm(FEHMotor::Motor2,9.0);
 
 void move_forward(int percent, int counts) //using encoders
 {
@@ -27,18 +31,18 @@ void move_forward(int percent, int counts) //using encoders
     left_encoder.ResetCounts();
 
     //Set both motors to desired percent
-    right_motor.SetPercent(percent);
-    left_motor.SetPercent(-1 * percent);
+    right_motor.SetPercent(-1 * percent);
+    left_motor.SetPercent(percent);
 
     while (left_encoder.Counts() < counts){
         LCD.WriteLine("forward");
-        Sleep(0.5);
+        Sleep(1.0);
     }
 
     //While the average of the left and right encoder is less than counts,
     //keep running motors
     while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
-    
+
     //Turn off motors
     right_motor.Stop();
     left_motor.Stop();
@@ -51,18 +55,18 @@ void move_backward(int percent, int counts) //using encoders
     left_encoder.ResetCounts();
 
     //Set both motors to desired percent
-    right_motor.SetPercent(-1 * percent);
-    left_motor.SetPercent(percent);
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(-1 * percent);
 
     while (left_encoder.Counts() < counts){
         LCD.WriteLine("backward");
-        Sleep(0.5);
+        Sleep(1.0);
     }
 
     //While the average of the left and right encoder is less than counts,
     //keep running motors
     while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
-
+    
     //Turn off motors
     right_motor.Stop();
     left_motor.Stop();
@@ -88,9 +92,7 @@ right_motor.SetPercent(percent);
 
 while (((left_encoder.Counts() + right_encoder.Counts()) / 2) < counts) {
     LCD.WriteLine("Left");
-    Sleep(0.5);
-    left_motor.SetPercent(percent);
-    right_motor.SetPercent(percent);
+    Sleep(1.0);
 }
 
 //Turn off motors
@@ -114,17 +116,15 @@ left_encoder.ResetCounts();
 
 //hint: set right motor backwards, left motor forwards
 
-left_motor.SetPercent(percent);
-right_motor.SetPercent(percent);
+left_motor.SetPercent(-1 * percent);
+right_motor.SetPercent(-1 * percent);
 
 //While the average of the left and right encoder is less than counts,
 //keep running motors
 
 while (((left_encoder.Counts() + right_encoder.Counts()) / 2) < counts) {
     LCD.WriteLine("Right");
-    Sleep(0.5);
-    left_motor.SetPercent(percent);
-    right_motor.SetPercent(percent);
+    Sleep(1.0);
 }
 
 //Turn off motors
@@ -171,84 +171,67 @@ void lineTracking(float fast_motor_percent, float slow_motor_percent){
     }
 }
 
+void move_bucket_arm(int percent, float seconds){
+
+    //Set desired motor percentage
+    bucket_arm.SetPercent(percent);
+
+    float time = TimeNow();
+
+    //move thr arm for a certain amoutn of time
+    while(time - TimeNow() < seconds){
+    if(percent > 0){
+        LCD.WriteLine("Dropping bucket");
+        Sleep(0.5);
+    }
+    else {
+        LCD.WriteLine("Raising bucket");
+        Sleep(0.5);
+    }
+    
+    //Stop the arm
+    bucket_arm.Stop();
+    }
+}
+
 int main(void)
 {
-    //Initialize testing speed
-    int testSpeed = 25;
-    int ninetyDegreeCount = 265;
-
+    LCD.WriteLine("TESTING TESTING 1 2 3");
     //Check if the starting light is not red
     while (cds.Value() <= 0.3 || cds.Value() >= 0.7) {
         left_motor.Stop();
         right_motor.Stop();
         LCD.WriteLine(cds.Value());
-        Sleep(0.5);
+        Sleep(1.0);
     }
 
-    //Move forward
-    move_forward(testSpeed, 240);
-
+    //Move to trash can
+    move_forward(testSpeed, 250); //move forward from starting light
     Sleep(1.0);
-
-    //Make a slight turn left
-    turn_left(testSpeed, 135);
-
+    turn_left(testSpeed, 60); //make a slight turn
     Sleep(1.0);
+    move_forward(testSpeed, 350); //move forward to be level with trash can
+    Sleep(1.0);
+    turn_right(testSpeed, ninetyDegreeCount); //make a 90 degree turn right
+    Sleep(1.0);
+    move_forward(testSpeed, 400); //move forward to trash can
 
-    //Move forward
-    move_forward(testSpeed, 260);
+    //Dump tray into trash can
+    move_bucket_arm(testSpeed, 2); //move arm down
+    Sleep(1.0);
+    move_bucket_arm(-1 * testSpeed, 2); //retract arm back up
 
-    //Check if the light is red
-    if (cds.Value() > 0.3 && cds.Value() < 0.7){
-        left_motor.Stop();
-        right_motor.Stop();
-        LCD.WriteLine("RED detected");
-        turn_left(testSpeed, ninetyDegreeCount); //Make a 90 degree turn 
-        Sleep(1.0);
-        move_forward(testSpeed, 10); //press the button
-        Sleep(1.0);
-        move_backward(testSpeed, 50); //move back from the button 
-        Sleep(1.0);
-        turn_left(testSpeed, ninetyDegreeCount); //make a 90 degree turn left
-        Sleep(1.0);
-    }
-
-    //Check if light is blue
-    else if (cds.Value() > 0.8 && cds.Value() < 1.2){
-        left_motor.Stop();
-        right_motor.Stop();
-        LCD.WriteLine("BLUE detected");
-        Sleep(1.0);
-        turn_left(testSpeed, ninetyDegreeCount); //make a 90 degree turn left
-        Sleep(1.0);
-        move_forward(testSpeed, 10); //press the button
-        Sleep(1.0);
-        move_backward(testSpeed, 50); //move back from the button
-        Sleep(1.0);
-        turn_left(testSpeed, ninetyDegreeCount); //make a 90 degree turn left
-        Sleep(1.0);
-    }
-
-    else {
-        LCD.WriteLine("RED");
-        left_motor.Stop();
-        right_motor.Stop();
-    } 
-
-    move_forward(testSpeed, 100); //Move close to the ramp
-
-    turn_left(testSpeed, ninetyDegreeCount); //turn left to face the ramp
+    // //Move up the ramp
+    // move_backward(testSpeed, 100); //move back from trash can
+    // Sleep(1.0);
+    // turn_right(testSpeed, ninetyDegreeCount); //turn 90 dgerees to move to the ramp
+    // Sleep(1.0);
+    // move_forward(testSpeed, 200); //move closer to the ramp
+    // Sleep(1.0);
+    // turn_left(testSpeed, ninetyDegreeCount); //turn to face the ramp
+    // Sleep(1.0);
+    // move_forward(testSpeed, 500); //move up the ramp
     
-    Sleep(1.0);
-
-    move_forward(testSpeed, 1000); //move up the ramp
-
-    Sleep(1.0);
-    
-    move_backward(testSpeed, 1000); //move down the ramp
-
-    Sleep(1.0);
-
     LCD.WriteLine("Hell yeah");
 
 }
